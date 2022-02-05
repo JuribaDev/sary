95f05bdf4825a5094ec2990d5e667ba048fa24b0
juriba <juribaalsiari@gmail.com>
2022-02-05 18:42:23 +0300

    go to ios side

--- lib/app/common/native_plugn/custom_toast_message.dart
+++ lib/app/common/native_plugn/custom_toast_message.dart
@@ -1,16 +1,8 @@
 import 'dart:async';
-import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 class SaryToastMessage {
   static const MethodChannel channel = MethodChannel('com.sary.juriba/toast');
-
-  // static Future<bool> cancel() async {
-  //   bool res = await channel.invokeMethod("cancel");
-  //   return res;
-  // }
-
   static Future<void> showToast({
     required String message,
   }) async {
